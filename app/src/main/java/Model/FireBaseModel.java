051e8f17eb0e16b94deb939c79fe84487a3be1db
manor <manor.erel20@gmail.com>
2020-04-05 16:17:50 +0300

    add firebase connection

---
+++ app/src/main/java/Model/FireBaseModel.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.Map;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FireBaseModel {
+
+    public static void SaveRecipe(Map<String, Object> recipe){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Recipe");
+        myRef.setValue(recipe);
+    }
+
+}
