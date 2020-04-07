2941f0bcc8a7e6a07eb873fa56d90dfed48a0a98
manor <manor.erel20@gmail.com>
2020-04-07 12:31:45 +0300

    add google loggin button

--- app/src/main/java/com/example/mycookbook/loggin.java
+++ app/src/main/java/com/example/mycookbook/loggin.java
@@ -1,5 +1,7 @@
 package com.example.mycookbook;
 
+import Model.Statics;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,12 +9,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.tasks.Task;
 
 public class loggin extends AppCompatActivity implements View.OnClickListener {
@@ -27,11 +33,20 @@ public class loggin extends AppCompatActivity implements View.OnClickListener {
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-//        updateUI(account);
+
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account!= null){
+            Statics.userId = account.getId();
+            finish();
+        }
     }
 
     @Override
@@ -40,6 +55,8 @@ public class loggin extends AppCompatActivity implements View.OnClickListener {
             case R.id.sign_in_button:
                 signIn();
                 break;
+            default:
+                finish();
             // ...
         }
 
@@ -63,17 +80,28 @@ public class loggin extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
+    private void handleAcount(){
+//        Task<GoogleSignInAccount> googleSignInAccountTask = mGoogleSignInClient.silentSignIn();
+//        handleSignInResult(googleSignInAccountTask);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        onActivityResult(2,0,signInIntent);
+    }
+
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-//            updateUI(account);
+            Statics.userId = account.getId();
+            finish();
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w("", "signInResult:failed code=" + e.getStatusCode());
+            Log.println(1,"", "signInResult:failed code=" + e.getStatusCode());
+//            handleAcount();
 //            updateUI(null);
+        }
+        catch (Exception e){
+            Log.w("", e.getMessage());
         }
     }
 }
