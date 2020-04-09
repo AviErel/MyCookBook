961fb0cafcc3c83ded0c869711cd296f070c9fb2
manor <manor.erel20@gmail.com>
2020-04-09 19:51:57 +0300

    design

--- app/src/main/java/com/example/mycookbook/Base.java
+++ app/src/main/java/com/example/mycookbook/Base.java
@@ -1,7 +1,9 @@
 package com.example.mycookbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,10 +18,16 @@ public class Base extends AppCompatActivity {
         setContentView(R.layout.activity_base);
     }
 
+    @SuppressLint("RestrictedApi")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mainmenu, menu);
+
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
         return true;
     }
 
